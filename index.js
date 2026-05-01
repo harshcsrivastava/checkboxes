dbd21c1099e84480b7d7f8bb1547ccb12edd5c2d
Harsh Srivastava <harshcsrivastava@gmail.com>
2026-05-01 19:18:26 +0530

    feat: integrate Redis for checkbox state management and add Docker support

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ import http from "node:http";
 import express from "express";
 import { Server } from "socket.io";
 
-const CHECKBOX_COUNT = 100;
+import { redis, publisher, subscriber } from "./redis-connection.js";
 
-const state = {
-    checkbox: new Array(CHECKBOX_COUNT).fill(false),
-};
+const CHECKBOX_COUNT = 50;
+const CHECKBOX_STATE_KEY = "checkbox-state";
+
 async function main() {
     const PORT = process.env.PORT ?? 9000;
 
@@ -19,15 +19,43 @@ async function main() {
     const io = new Server();
     io.attach(server);
 
+    await subscriber.subscribe("internal-server:checkbox:change"); // is channel ko subscribe kiya
+    subscriber.on("message", (channel, message) => {
+        if (channel === "internal-server:checkbox:change") {
+            const { index, checked } = JSON.parse(message);
+            // state.checkbox[index] = checked; //updating state
+            io.emit("server:checkbox:change", { index, checked });
+
+            //problem ye thi jo baad me join karega vapas usme state nhi rahegi, therefor state ko bhi publish krdo on redis
+            // Make redis single-source of TRUTH
+            // so set the data in redis using GET and SET
+        }
+    });
+
     // SOCKET IO HANDLER
     io.on("connection", (socket) => {
         console.log(`Socket connected: ${socket.id}`);
 
-        socket.on("client:checkbox:change", (data) => {
-            console.log(data);
-            io.emit("server:checkbox:change", data);
-            //updating server state after refresh
-            state.checkbox[data.index] = data.checked;
+        socket.on("client:checkbox:change", async (data) => {
+            const existingState = await redis.get(CHECKBOX_STATE_KEY);
+
+            if (existingState) {
+                const remoteData = JSON.parse(existingState);
+                remoteData[data.index] = data.checked;
+                await redis.set(CHECKBOX_STATE_KEY, JSON.stringify(remoteData));
+            } else {
+                await redis.set(
+                    CHECKBOX_STATE_KEY,
+                    JSON.stringify(new Array(CHECKBOX_COUNT).fill(false)),
+                );
+            }
+
+            // user kuch bhi batayega to use REDIS ko batao rathher than emiting, forward it to redis
+            // publish kar diya
+            publisher.publish(
+                "internal-server:checkbox:change",
+                JSON.stringify(data),
+            );
         });
     });
 
@@ -38,9 +66,14 @@ async function main() {
         return res.json({ message: "Still Alive" });
     });
 
-    app.get("/checkbox", (req, res) => {
+    app.get("/checkbox", async (req, res) => {
+        const existingState = await redis.get(CHECKBOX_STATE_KEY);
+        if (existingState) {
+            const remoteData = JSON.parse(existingState);
+            return res.json({checkboxes: remoteData})
+        }
         return res.json({
-            checkboxes: state.checkbox,
+            checkboxes: new Array(CHECKBOX_COUNT).fill(false)
         });
     });
 
